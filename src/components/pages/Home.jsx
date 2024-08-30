19cf77bfc81356177c056ea3dc6d4fca24d80b47
Rodrigo Alves <alvesrodrigo762@gmail.com>
2024-08-30 11:16:32 -0300

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import Hero from '../Hero';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
